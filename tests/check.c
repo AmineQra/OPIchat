b91e00b7f557a1a985d95b5c55d21a791710df19
Amine Ahdadouch <amine.ahdadouch@epita.fr>
2022-02-22 22:17:39 +0100

    opichat project (IRC)

--- tests/check.c
+++
@@ -1,8 +0,0 @@
-#include "../src/utils/parsing.h"
-
-int main(void)
-{
-    int actual = char_matching_pattern("amine123", "^[a-zA-Z0-9]*$");
-    int expected = 1;
-    return actual == expected;
-}
